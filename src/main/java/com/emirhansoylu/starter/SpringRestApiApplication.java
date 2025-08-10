344d4827323b6c3152035c3b9dd32f3220691ac4
t <t@DESKTOP-KDLNPJ1>
2025-08-10 14:05:15 +0300

    Second Commit

--- src/main/java/com/emirhansoylu/starter/SpringRestApiApplication.java
+++ src/main/java/com/emirhansoylu/starter/SpringRestApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.enesbayram") //com.enesbayram paketi içini tara diyoruz. beanı oluştur diyoruz
+@ComponentScan(basePackages = "com.emirhansoylu") //com.enesbayram paketi içini tara diyoruz. beanı oluştur diyoruz
 @SpringBootApplication
 public class SpringRestApiApplication {
 
